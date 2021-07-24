217d5e6c92c2725747bb215b68e7b8ab1429a825
nasum <tomato.wonder.life@gmail.com>
2021-07-25 01:11:52 +0900

    refactor cmd

--- cmd/ghs/main.go
+++ cmd/ghs/main.go
@@ -13,18 +13,6 @@ func main() {
 	ctx := context.Background()
 	client := lib.NewClient(ctx)
 
-	prCmd := flag.NewFlagSet("pr", flag.ExitOnError)
-	prOwner := prCmd.String("owner", "", "owner name")
-	prRepo := prCmd.String("repo", "", "repository")
-	prFrom := prCmd.String("from", "", "from branch")
-	prTo := prCmd.String("to", "", "to branch")
-
-	diffCmd := flag.NewFlagSet("diff", flag.ExitOnError)
-	diffOwner := diffCmd.String("owner", "", "owner name")
-	diffRepo := diffCmd.String("repo", "", "repository")
-	diffFrom := diffCmd.String("from", "", "from branch")
-	diffTo := diffCmd.String("to", "", "to branch")
-
 	if len(os.Args) < 2 {
 		fmt.Println("help")
 		os.Exit(0)
@@ -32,27 +20,37 @@ func main() {
 
 	switch os.Args[1] {
 	case "pr":
-		prCmd.Parse(os.Args[2:])
+		cmd := flag.NewFlagSet("pr", flag.ExitOnError)
+		owner := cmd.String("owner", "", "owner name")
+		repo := cmd.String("repo", "", "repository")
+		from := cmd.String("from", "", "from branch")
+		to := cmd.String("to", "", "to branch")
+		cmd.Parse(os.Args[2:])
 
-		title, body, err := lib.BranchDiff(ctx, client, *prOwner, *prRepo, *prFrom, *prTo)
-
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
-			os.Exit(1)
-		}
-
-		prUrl, err := lib.CreatePullRequest(ctx, client, *prOwner, *prRepo, *prFrom, *prTo, title, body)
+		title, body, err := lib.BranchDiff(ctx, client, *owner, *repo, *from, *to)
 
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
 			os.Exit(1)
 		}
 
-		fmt.Printf("PullRequest Url: %v\n", prUrl)
+		url, err := lib.CreatePullRequest(ctx, client, *owner, *repo, *from, *to, title, body)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("PullRequest Url: %v\n", url)
 
 	case "diff":
-		diffCmd.Parse(os.Args[2:])
-		title, body, err := lib.BranchDiff(ctx, client, *diffOwner, *diffRepo, *diffFrom, *diffTo)
+		cmd := flag.NewFlagSet("diff", flag.ExitOnError)
+		owner := cmd.String("owner", "", "owner name")
+		repo := cmd.String("repo", "", "repository")
+		from := cmd.String("from", "", "from branch")
+		to := cmd.String("to", "", "to branch")
+		cmd.Parse(os.Args[2:])
+		title, body, err := lib.BranchDiff(ctx, client, *owner, *repo, *from, *to)
 
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
